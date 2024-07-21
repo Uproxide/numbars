d2c9a834f85fe3d8b8e377865c1de7e13d79aedc
Uproxide <153382689+Uproxide@users.noreply.github.com>
2024-07-21 18:54:00 -0400

    add a check for dividing by 0

--- numbars/__init__.py
+++ numbars/__init__.py
@@ -10,7 +10,10 @@ def multiply_numbers(one, two):
     return one * two
 
 def divide_numbers(one, two):
-    return one / two
+    if (one == 0 or two == 0) or (one == 0 and two == 0): 
+        return "Can't Divide by 0!"
+    else:
+        return one / two
 
 def randomize_numbers(min, max):
     return random.randint(min, max)
